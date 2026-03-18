ec10075683d71ab186b6c26035c60e04133e5ffb
JaeHyeokSim <01088825238sjh@gmail.com>
2026-03-18 15:38:43 +0900

    Add missing require_once statements

--- example/example.php
+++ example/example.php
@@ -1,10 +1,15 @@
 <?php
 
-require_once "../src/Scraper.php";
+require_once __DIR__ . '/../src/Scraper.php';
+require_once __DIR__ . '/../src/Parser.php';
+
+require_once __DIR__ . '/../../php-http-client/src/HttpClient.php';
+require_once __DIR__ . '/../../php-cache-library/src/MemoryCache.php';
+require_once __DIR__ . '/../../php-rate-limiter/src/RateLimiter.php';
 
 $httpClient = new HttpClient();
 $cache = new MemoryCache();
-$rateLimiter = new RateLimiter(1); // 초당 1회
+$rateLimiter = new RateLimiter();
 
 $scraper = new Scraper($httpClient, $cache, $rateLimiter);
 
